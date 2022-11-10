77e914059a66f9f18da79340d09783578d2f89b0
Red Oral <redmunozoral@gmail.com>
2022-11-09 19:27:08 -0800

    Added skills on About page

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,7 +1,19 @@
 import styles from './About.module.sass';
 import { AiOutlineFilePdf } from 'react-icons/ai';
+import { GoChevronDown, GoChevronUp } from 'react-icons/go';
+import { useState } from 'react';
 
 const About: React.FC = () => {
+  const [display, setDisplay] = useState('none');
+
+  const toggleCollapse = () => {
+    if (display === 'none') {
+      setDisplay('flex');
+    } else if (display === 'flex') {
+      setDisplay('none');
+    }
+  };
+
   return (
     <section className={styles.container}>
       <h2>about me</h2>
@@ -18,6 +30,61 @@ const About: React.FC = () => {
           }}
         />
       </button>
+
+      <span className={styles.expandButton} onClick={() => toggleCollapse()}>
+        {display === 'none' ? (
+          <span>
+            more <GoChevronDown style={{ transform: 'translateY(2px)' }} />
+          </span>
+        ) : (
+          <span>
+            collapse <GoChevronUp style={{ transform: 'translateY(2px)' }} />
+          </span>
+        )}
+      </span>
+      <div className={styles.skills} style={{ display: display }}>
+        <div>
+          <h3>frontend</h3>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>SASS</li>
+            <li>Bootstrap</li>
+            <li>JavaScript</li>
+            <li>TypeScript</li>
+            <li>React</li>
+            <li>React Native</li>
+            <li>Redux</li>
+          </ul>
+        </div>
+        <div>
+          <h3>backend</h3>
+          <ul>
+            <li>Node.js</li>
+            <li>Express.js</li>
+            <li>Java</li>
+            <li>Spring Boot</li>
+            <li>PHP</li>
+            <li>MySQL</li>
+            <li>NoSQL</li>
+            <li>PostgreSQL</li>
+            <li>AWS Serverless</li>
+          </ul>
+        </div>
+        <div>
+          <h3>other</h3>
+          <ul>
+            <li>Jest</li>
+            <li>Enzyme</li>
+            <li>JUnit</li>
+            <li>Git</li>
+            <li>Docker</li>
+            <li>VSCode</li>
+            <li>Agile</li>
+            <li>Scrum</li>
+          </ul>
+        </div>
+      </div>
     </section>
   );
 };
