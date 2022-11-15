36fe92464362cbd4a48f433a820d919f6303132b
Red Oral <redmunozoral@gmail.com>
2022-11-15 04:00:13 -0800

    Replaced map with a temporary image  on contact, adjustments to contact and footer sizing

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -71,11 +71,7 @@ const Contact: React.FC<RefsI> = (props: RefsI) => {
 
   return (
     <section className={styles.contactContainer} ref={props.refs.contactRef}>
-      <iframe
-        src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d196868.24580219598!2d-119.9909243034391!3d39.55811167610205!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x809940ae9292a09d%3A0x40c5c5ce7438f787!2sReno%2C%20NV!5e0!3m2!1sen!2sus!4v1668114398416!5m2!1sen!2sus'
-        className={styles.map}
-        loading='lazy'
-        referrerPolicy='no-referrer-when-downgrade'></iframe>
+      <div className={styles.personalImg}></div>
       <div className={styles.form}>
         <h2>contact me</h2>
         <span className={styles.contactInfo}>
